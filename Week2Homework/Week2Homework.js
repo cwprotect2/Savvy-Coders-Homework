2d7fd8e77ddb2f4637761a22b74dd25ed81d3680
Corwin Watson <77928776+cwprotect2@users.noreply.github.com>
2021-02-10 17:21:28 -0600

    update to Homeworkweek2

---
+++ Week2Homework/Week2Homework.js
@@ -0,0 +1,18 @@
+let PizzaPlace;
+let NumberOfToppings;
+
+PizzaPlace = "Corwin's Pizza";
+NumberOfToppings = 8;
+
+console.log(PizzaPlace, typeof PizzaPlace);
+
+console.log(NumberOfToppings, typeof NumberOfToppings);
+
+
+console.log(`At my restaurant ${PizzaPlace}, i have ${NumberOfToppings} different toppings `);
+
+/* if (NumberOfToppings is <=8) {
+  //  That's a Nice size Pizza
+  };
+if (NumberOfToppings is >=8) {That's a whole lot of Pizza};
+*/
